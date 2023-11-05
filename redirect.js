3ced41804b19801114fede004cde621392492e9e
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2023-11-05 10:04:31 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,16 @@
 if(self == top){
     window.location.href = '/?page=' + window.location.pathname.split('/')[2].split('.')[0]
 }
-document.querySelector('head').innerHTML = '<link rel="stylesheet" type="text/css" href="https://bananacake.top/css/index.css">'
+document.head.innerHTML = `<link rel="stylesheet" type="text/css" href="https://bananacake.top/css/index.css">
+<script type="text/javascript">
+    (function(){
+        emailjs.init("xj8nyEySYwVrb0Kxc");
+    })();
+</script>`
+fetch('https://api.uomg.com/api/visitor.info?skey=114514')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        emailjs.send("service_1xi18hh", "template_qxwn03w", {page: window.location.search, message: data, time: new Date().toString()});
+    })
