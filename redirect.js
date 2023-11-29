67d1469a5800f3c24a35a3fd201bd538e85a2123
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2023-11-30 07:17:45 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,5 +2,3 @@ if(self == top){
     window.location.href = '/?page=' + window.location.pathname.split('/')[2].split('.')[0]
 }
 document.head.innerHTML = '<link rel="stylesheet" type="text/css" href="https://bananacake.top/css/index.css">'
-document.querySelectorAll('img').forEach(function(ele){ele.style.cssText="width: 100%;"})
-console.log(document.querySelectorAll('img'))
