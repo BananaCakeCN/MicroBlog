c35961dffe2b3512e1e0c41ceaec5d6af79cbefb
加卷 <103292046+BananaCakeCN@users.noreply.github.com>
2025-01-29 13:07:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ var page = 'main'
 if(new URL(document.location).searchParams.get('page')!=null){
     page = new URL(document.location).searchParams.get('page')
     document.getElementsByClassName('pageContent')[0].innerHTML = '<iframe class="iframeBox" src="https://microblog.bananacake.top/pages/' + page + '/"></iframe>'
-    fetch('http://api.uomg.com/api/visitor.info?skey=-1')
+    fetch('https://2025.ipchaxun.com/')
         .then(function(response) {
             return response.json();
         })
